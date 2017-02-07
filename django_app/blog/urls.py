d1cd8464e38ab309e63f3124a5a74f6d56f283c7
LimHM <gusals3407@gmail.com>
2017-02-07 16:16:29 +0900

    템플릿 확장을 사용하도록 개선

--- django_app/blog/urls.py
+++ django_app/blog/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.post_list, name='post_list'),
-    url(r'^post/(?P<post_id>[0-9]+)/', views.post_detail, name='post_id'),
+    url(r'^post$', views.post_list, name='post_list'),
+    url(r'^post/detail/(?P<post_id>[0-9]+)$', views.post_detail, name='post_detail'),
 ]
