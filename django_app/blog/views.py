f65d5ae1517a94e32bf2578aea4ef587e99ead89
LimHM <gusals3407@gmail.com>
2017-02-07 15:56:50 +0900

    post_list와 post_detail을 이동하는 링크 생성

--- django_app/blog/views.py
+++ django_app/blog/views.py
@@ -1,9 +1,31 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.utils import timezone
 from .models import Post
+from django.shortcuts import get_object_or_404
 
 
 def post_list(request):
     context = {
-        'post_list': Post.objects.all(),
+        'post_list': Post.objects.filter(
+            published_date__lte=timezone.now()
+        ),
     }
     return render(request, 'blog/post-list.html', context)
+
+
+def post_detail(request, post_id):
+    # try:
+    #     # ORM을 이용해서 id가 전달받은 post_id와 일치하는 Post객체를 post변수에 할당
+    #     post = Post.objects.get(id=post_id)
+    # except Post.DoesNotExist as e:
+    #     return HttpResponse(e)
+
+    post = get_object_or_404(Post, id=post_id)
+
+    # 전달할 context 딕셔너리 키 'post'에 post 변수를 전달
+    context = {
+        'post': post,
+    }
+    # blog/post-detail.html 템플릿을 render한 결과를 리턴
+    return render(request, 'blog/post-detail.html', context)
