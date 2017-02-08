e04eec88c2408982ab5e5ef55c70f8a7b8326a81
LimHM <gusals3407@gmail.com>
2017-02-08 14:58:43 +0900

    post 추가 기능

--- django_app/blog/urls.py
+++ django_app/blog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^post$', views.post_list, name='post_list'),
-    url(r'^post/detail/(?P<post_id>[0-9]+)$', views.post_detail, name='post_detail'),
+    url(r'^post/detail/(?P<post_id>[0-9]+)/$', views.post_detail, name='post_detail'),
+    url(r'^post/add/$', views.post_add, name='post_add')
 ]
--- django_app/blog/views.py
+++ django_app/blog/views.py
@@ -1,5 +1,6 @@
+from django.contrib.auth.models import User
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.utils import timezone
 from .models import Post
 from django.shortcuts import get_object_or_404
@@ -7,9 +8,10 @@ from django.shortcuts import get_object_or_404
 
 def post_list(request):
     context = {
-        'post_list': Post.objects.filter(
-            published_date__lte=timezone.now()
-        ),
+        # 'post_list': Post.objects.filter(
+        #     published_date__lte=timezone.now()
+        # ),
+        'post_list': Post.objects.all()
     }
     return render(request, 'blog/post-list.html', context)
 
@@ -29,3 +31,36 @@ def post_detail(request, post_id):
     }
     # blog/post-detail.html 템플릿을 render한 결과를 리턴
     return render(request, 'blog/post-detail.html', context)
+
+
+def post_add(request):
+    if request.method == 'POST':
+        # 요청의 method가 POST일 경우
+        # 요청받은 데이터를 출력
+        data = request.POST
+        # html의 name이 키값
+        title = data['input_title']
+        content = data['input_content']
+        author = User.objects.get(id=1)
+        # print(request.POST)
+        # ret = ','.join([title, content])
+
+        # 받은 데이터를 사용해세 Post 객체를 생성
+        p = Post(title=title, content=content, author=author)
+        p.save()
+
+        # redirect메서드는 인자로 주어진
+        # URL 또는
+        # urlpattern의 name을 이용해 만들어낸 URL을 사용해서
+        # 브라우저가 해당 URL로 이동하도록 해줌
+        # 브라우저가 해당 URL로 이동하도록 해줌
+        # return redirect('post_list')
+
+        # 글 상세화면으로 이동 키워드(post_id)의 인자로 p.id를 전달
+        return redirect('post_detail', post_id=p.id)
+
+
+    else:
+        # 요청의 method가 POST가 아닐 경우
+        # 글 쓰기 양식이 있는 템플릿을 렌더해서 리턴
+        return render(request, 'blog/post-add.html')
